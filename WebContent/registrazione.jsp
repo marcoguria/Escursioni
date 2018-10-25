7b4c936b5fb7db13d2845a02b29ec913651d2758
eleonoramascia11 <eleonoramascia11@gmail.com>
2018-10-25 12:17:46 +0200

    creta RegistrazioneServlet

---
+++ WebContent/registrazione.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form method="POST" action="RegistrazioneServlet">
+		
+		Nome: <input type="text" name="nome"/>
+		
+		<br>
+		
+		Cognome: <input type="text" name="cognome"/>
+		
+		<br>
+		CodFiscale: <input type="text" name="codf"/>
+		
+		<br>
+		
+		Email: <input type="text" name="email"/>
+		
+		<br>
+		
+		<!-- DataNascita: <input type="date" name="dataNascita"/> -->
+		<br>
+		
+		Username: <input type="text" name="username"/>
+		
+		<br>
+		
+		Password: <input type="password" name="password"/>
+		
+		<br>
+		
+		<input type="submit" value="REGISTRATI"/>
+		
+	</form>
+</body>
+</html>
---
+++ src/it/ats/controllo/RegistrazioneServlet.java
@@ -0,0 +1,67 @@
+package it.ats.controllo;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.ats.modello.Cliente;
+import it.ats.modello.UtenteRegistrato;
+import it.ats.persistenza.DAOException;
+import it.ats.persistenza.DAOGuest;
+import it.ats.persistenza.DAOUtenteRegistrato;
+import it.ats.persistenza.impl.DAOGuestImpl;
+import it.ats.persistenza.impl.DAOUtenteRegistratoImpl;
+
+/**
+ * Servlet implementation class RegistrazioneServlet
+ */
+@WebServlet("/RegistrazioneServlet")
+public class RegistrazioneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrazioneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		UtenteRegistrato cliente = new Cliente();
+		
+		cliente.setNome(request.getParameter("nome"));
+		cliente.setCognome(request.getParameter("cognome"));
+		cliente.setCodf(request.getParameter("codf"));
+		cliente.setEmail(request.getParameter("email"));		
+		//cliente.setData_nascita(new Date(1985,2,23));		
+		cliente.setFlag_ruolo(1);				
+		cliente.setUsername(request.getParameter("username"));
+		cliente.setPassword(request.getParameter("password"));	
+		
+		DAOGuest daoGuest= new DAOGuestImpl();
+		try {
+			daoGuest.registrazione(cliente);
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+		
+		doGet(request, response);
+	}
+
+}
